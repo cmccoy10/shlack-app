6a18c2fc8e3ed953e4ab8c4631291be0a813d1f5
Cole McCoy <cole.mccoy41@hotmail.com>
2020-12-30 00:55:44 -0600

    Updated seeders

--- db/seeders/20201027024450-default-channels.js
+++ db/seeders/20201027024450-default-channels.js
@@ -3,9 +3,9 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
       return queryInterface.bulkInsert('Channels', [
-        {title: "2020-07-13-circle-4", topic: "Circle 4!", ownerId: 1, createdAt: new Date(), updatedAt: new Date() },
-        {title: "2020-07-13-online", topic: "July 2020 Cohort", ownerId: 1, createdAt: new Date(), updatedAt: new Date() },
-        {title: "TheChosenOnes", topic: "Relax and chat", ownerId: 2, createdAt: new Date(), updatedAt: new Date() },
+        {title: "project-phoenix", topic: "Share your progress and stay informed", ownerId: 2, createdAt: new Date(), updatedAt: new Date() },
+        {title: "morning-standup", topic: "Morning Standup Meetings @ 8", ownerId: 2, createdAt: new Date(), updatedAt: new Date() },
+        {title: "announcements", topic: "Announcements will be posted here first before emailed.", ownerId: 2, createdAt: new Date(), updatedAt: new Date() },
     ], {});
   },
 
